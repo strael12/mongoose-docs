f9ca7e02ca7f4d82f20235fc3d6ec1601c91e48c
Rafael Macedo <rafaelmacedosilva88@hotmail.com>
2022-08-28 21:02:47 -0300

    Update list e delete atualizado

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const mongoose = require('mongoose');
 const PASS = '88488263';
 const USER = 'rafaelsilva2';
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 const Client = require('./Model/Schema/Client');
 
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')));
 
 mongoose.connect(`mongodb+srv://${USER}:${PASS}@treinamento.usistsp.mongodb.net/?retryWrites=true&w=majority`, () => {
     console.log('Connected');
@@ -39,10 +41,56 @@ app.post('/create', async (req,res,next) => {
             endereco: endereco
         })
 
-        res.render('home');
+        res.redirect('/read');
     }else {
         res.render('error');
     }
 })
+
+// Ler e lista todos os clientes
+app.get('/read', async (req,res,next) => {
+    const all = await Client.find();// Retorna todos os clientes
+    res.render('list', {clients: all});
+})
+
+
+
+// Recupera os valores nos campos do input 
+app.get('/update/:id', async (req,res,next) => {
+    const client = await Client.findOne({_id: req.params.id});
+    res.render('edit', {clients: client});
+})
+
+// Atualiza no banco de dados assim que é feito o post
+app.post('/update', async (req,res,next) => {
+    const clientId = req.body.id;
+
+    if(clientId){
+        // Primeiro parametro filtra
+        // Segundo parametro pegar os valores recuperados e atualiza no banco de dados
+        await Client.updateOne({_id: req.body.id},{
+            nome: req.body.nome,
+            email: req.body.email,
+            telefone: req.body.telefone,
+            endereco: req.body.endereco})
+        res.redirect('/read');
+    }else {
+        // Se não existir o ID é gerado um erro
+        res.status(404).render('error');
+    }
+})
+
+
+// O link está na página de listagem, assim que o usuário clica em deletar 
+// usuário, ele recebe o parametro e compara como deleteOne, se for igual 
+// e feita a remoção e após isso, voltar para a mesma página.
+app.get('/delete/:id', async (req,res,next) => {
+    const id = req.params.id;   
+    if(id){
+        await Client.deleteOne({_id: id});
+        res.redirect('/read');
+    }
+})
+
 
 app.listen(PORT, () => console.log('The server is running at localhost:3000'));
